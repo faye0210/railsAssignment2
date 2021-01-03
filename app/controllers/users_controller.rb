244fe9d83c7efad8c0df9161adad8a2900d3d067
Faye.H <yang5chen12hui@gmail.com>
2021-01-03 23:32:11 +0900

    プロフィールの編集機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,18 @@ class UsersController < ApplicationController
       render :new
     end
   end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "プロフィールを更新しました"
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
   def show
     @user = User.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  root to: 'sessions#new'
   resources :posting_pages
   resources :sessions, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create, :edit, :update, :show]
 end
