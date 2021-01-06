8ad7001a1599efc75877ffb8f640f9c3e80352db
Faye.H <yang5chen12hui@gmail.com>
2021-01-06 15:05:03 +0900

    プロフィール画像の表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   root to: 'sessions#new'
-  resources :posting_pages do
-    collection do
-      post :index
-    end
-  end
+  resources :posting_pages
   resources :sessions, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create, :edit, :update, :show]
+  resources :users, only: [:index, :new, :create, :edit, :update, :show]
 end
